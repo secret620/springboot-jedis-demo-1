ae09f59669b14e56b72843061e6e16fb5610fa05
shiyouqin <secret620@163.com>
2021-02-19 11:07:51 +0800

    jedis util

--- src/main/java/com/example/Demo.java
+++ src/main/java/com/example/Demo.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.JedisPool;
 
+import java.nio.charset.StandardCharsets;
+
 /**
  * @Description 表示 ：
  * @Author Shi You Qin
@@ -21,6 +23,51 @@ public class Demo {
 
     @Test
     public void test( ) {
+        String add = redisJedisUtil.set("xx", "v1");
+        System.out.println(add);
         System.out.println( redisJedisUtil.get("xx") );
+    }
+
+    @Test
+    public void test2( ) {
+        redisJedisUtil.set("x2".getBytes(StandardCharsets.UTF_8), "v2".getBytes(StandardCharsets.UTF_8), 2);
+        redisJedisUtil.del(2,"x2" );
+    }
+
+    @Test
+    public void test3(){
+        System.out.println(redisJedisUtil.expire("x2", 10, 3));
+    }
+
+    @Test
+    public void test4(){
+        System.out.println(redisJedisUtil.pexpire("x2",1000 * 10, 3));
+        System.out.println(redisJedisUtil.pttl("x2", 3));
+        redisJedisUtil.persist("x2",3);
+        System.out.println(redisJedisUtil.pttl("x2", 3));
+    }
+
+    @Test
+    public void test5(){
+        System.out.println(redisJedisUtil.expire("x2",10, 3));
+    }
+
+    @Test
+    public void test6(){
+        System.out.println(redisJedisUtil.setex("x3","v3",10, 3));
+    }
+
+    @Test
+    public void test7(){
+        redisJedisUtil.set("x4","Hello World", 3);
+        System.out.println(redisJedisUtil.get("x4", 3));
+        System.out.println(redisJedisUtil.setrange("x4","Redis",6, 3));
+        System.out.println(redisJedisUtil.get("x4", 3));
+    }
+
+    @Test
+    public void test8(){
+        redisJedisUtil.mset(3, new String[]{"x4","v4", "x2","v2"});
+        System.out.println(new String(redisJedisUtil.getSet("x4", "v44", 3), StandardCharsets.UTF_8));
     }
 }
--- src/main/java/com/example/RedisJedisUtil.java
+++ src/main/java/com/example/RedisJedisUtil.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.nio.charset.StandardCharsets;
+
 /**
  * @Description 表示 ：
  * @Author Shi You Qin
@@ -17,6 +19,17 @@ public class RedisJedisUtil {
     @Autowired
     private JedisPool jedisPool;
 
+    private Jedis jedis(int indexDb) throws Exception{
+        Jedis jedis  = jedisPool.getResource();
+        jedis.select(indexDb);
+        return jedis;
+    }
+
+    private Jedis jedis() throws Exception{
+        Jedis jedis  = jedisPool.getResource();
+        return jedis;
+    }
+
     public String get(String key){
         return this.get(key, 0);
     }
@@ -24,8 +37,26 @@ public class RedisJedisUtil {
     public String get(String key , int indexDb){
         Jedis jedis = null;
         try{
-            jedis = jedisPool.getResource();
-            jedis.select(indexDb);
+            jedis = this.jedis(indexDb);
+            return jedis.get(key);
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+    public byte[] get(byte[] key) {
+        return this.get(key, 0);
+    }
+
+    public byte[] get(byte[] key , int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
             return jedis.get(key);
         }catch (Exception e){
             e.printStackTrace();
@@ -36,4 +67,459 @@ public class RedisJedisUtil {
         }
         return null;
     }
+
+    public String set(String key, String value){
+        return this.set(key, value, 0);
+    }
+
+    public String set(String key, String value, int indexDb){
+        return this.set(key.getBytes(StandardCharsets.UTF_8), value == null? null : value.getBytes(StandardCharsets.UTF_8), indexDb);
+    }
+
+    public String set(byte[] key, byte[] value){
+       return this.set(key, value, 0);
+    }
+
+    public String set(byte[] key, byte[] value, int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.set(key, value);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return "0";
+    }
+
+    public String setex(String key, String value, int seconds){
+        return this.setex(key.getBytes(StandardCharsets.UTF_8), value == null? null : value.getBytes(StandardCharsets.UTF_8), seconds, 0);
+    }
+
+    public String setex(byte[] key, byte[] value, int seconds){
+        return this.setex(key, value, seconds, 0);
+    }
+
+    public String setex(String key, String value, int seconds, int indexDb){
+        return this.setex(key.getBytes(StandardCharsets.UTF_8), value == null? null : value.getBytes(StandardCharsets.UTF_8), seconds, indexDb);
+    }
+
+    public String setex(byte[] key, byte[] value, int seconds, int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.setex(key, seconds, value);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return "0";
+    }
+
+    public Long del(byte[] ... keys){
+       return this.del(0, keys);
+    }
+
+    public Long del(int indexDb, byte[] ... keys){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.del(keys);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return 0L;
+    }
+
+    public Long del(int indexDb, String ... keys){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.del(keys);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return 0L;
+    }
+
+    public Long del(String ... keys){
+        return this.del(0, keys);
+    }
+
+    public Long append(String key, String appendStr, int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.append(key, appendStr);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return 0L;
+    }
+
+    public Long append(String key, String appendStr){
+        return this.append(key, appendStr, 0);
+    }
+
+    public Boolean exists(String key, int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.exists(key);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return false;
+    }
+
+    public Boolean exists(String key){
+        return this.exists(key, 0);
+    }
+
+    public String flushDB(){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis();
+            return jedis.flushDB();
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+    public String flashAll(){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis();
+            return jedis.flushAll();
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+    public Long expire(String key, int seconds){
+        return this.expire(key, seconds, 0);
+    }
+
+    public Long expire(String key, int seconds, int indexDb){
+        return this.expire(key.getBytes(StandardCharsets.UTF_8), seconds, indexDb);
+    }
+
+    public Long expire(byte[] key, int seconds, int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.expire(key, seconds);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return 0L;
+    }
+
+    public Long expire(byte[] key, int seconds){
+        return this.expire(key,seconds, 0);
+    }
+
+    public Long pexpire(byte[] key, int milliseconds, int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.pexpire(key, milliseconds);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return 0L;
+    }
+
+    public Long pexpire(byte[] key, int milliseconds){
+        return this.pexpire(key, milliseconds, 0);
+    }
+
+    public Long pexpire(String key, int milliseconds, int indexDb){
+        return this.pexpire(key.getBytes(StandardCharsets.UTF_8), milliseconds, indexDb);
+    }
+
+    public Long pexpire(String key, int milliseconds){
+        return this.pexpire(key, milliseconds, 0);
+    }
+
+    public Long ttl(String key, int indexDb){
+        return this.ttl(key.getBytes(StandardCharsets.UTF_8), indexDb);
+    }
+
+    public Long ttl(String key){
+        return this.ttl(key, 0);
+    }
+
+    public Long ttl(byte[] key, int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.ttl(key);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return 0L;
+    }
+
+    public Long ttl(byte[] key){
+        return this.ttl(key, 0);
+    }
+
+    public Long pttl(byte[] key, int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.pttl(key);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return 0L;
+    }
+
+    public Long pttl(byte[] key){
+        return this.pttl(key, 0);
+    }
+
+    public Long pttl(String key, int indexDb){
+        return this.pttl(key.getBytes(StandardCharsets.UTF_8), indexDb);
+    }
+
+    public Long pttl(String key){
+        return this.pttl(key, 0);
+    }
+
+    public Long persist(byte[] key, int indexDb){
+        Jedis jedis = null;
+        Long result = 0L;
+        try{
+            jedis = this.jedis(indexDb);
+            result =  jedis.persist(key);
+        }catch (Exception e){
+            e.printStackTrace();
+            result = -1L;
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return result;
+    }
+
+    public Long persist(String key, int indexDb){
+        return this.persist(key.getBytes(StandardCharsets.UTF_8), indexDb);
+    }
+
+    public Long persist(String key){
+        return this.persist(key.getBytes(StandardCharsets.UTF_8), 0);
+    }
+
+    public Long persist(byte[] key){
+        return this.persist(key, 0);
+    }
+
+    public Long setrange(byte[] key, byte[] replaceStr, int offset, int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.setrange(key, offset, replaceStr);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return 0L;
+    }
+
+    public Long setrange(byte[] key, byte[] replaceStr, int offset){
+        return this.setrange(key, replaceStr, offset, 0);
+    }
+
+    public Long setrange(String key, String replaceStr, int offset, int indexDb){
+        return this.setrange(key.getBytes(StandardCharsets.UTF_8), replaceStr == null ? null:replaceStr.getBytes(StandardCharsets.UTF_8), offset, indexDb);
+    }
+
+    public Long setrange(String key, String replaceStr, int offset){
+        return this.setrange(key, replaceStr, offset, 0);
+    }
+
+    public String mset(int indexDb, String ... keyValues){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.mset(keyValues);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+    public String mset(String ... keyValues){
+        return this.mset(0, keyValues);
+    }
+
+    public String mset(int indexDb, byte[] ... keyValues){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.mset(keyValues);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+    public String mset(byte[] ... keyValues){
+        return this.mset(0, keyValues);
+    }
+
+    public Long msetnx(int indexDb, String ... keyValues){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.msetnx(keyValues);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+    public Long msetnx(String ... keyValues){
+        return this.msetnx(0, keyValues);
+    }
+
+    public Long msetnx(int indexDb, byte[] ... keyValues){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.msetnx(keyValues);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+    public Long msetnx(byte[] ... keyValues){
+        return this.msetnx(0, keyValues);
+    }
+
+    public byte[] getSet(byte[] key, byte[] value, int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.getSet(key,value);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+    public byte[] getSet(byte[] key, byte[] value){
+        return this.getSet(key, value, 0);
+    }
+
+    public byte[] getSet(String key, String value, int indexDb){
+        return this.getSet(key.getBytes(StandardCharsets.UTF_8), value == null ? null : value.getBytes(StandardCharsets.UTF_8), indexDb);
+    }
+
+    public byte[] getSet(String key, String value){
+        return this.getSet(key, value, 0);
+    }
+
+    public byte[] getrange(byte[] key, int startOffset, int endOffset, int indexDb){
+        Jedis jedis = null;
+        try{
+            jedis = this.jedis(indexDb);
+            return jedis.getrange(key, startOffset, endOffset);
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+    public byte[] getrange(String key, int startOffset, int endOffset, int indexDb){
+        return this.getrange(key.getBytes(StandardCharsets.UTF_8), startOffset, endOffset, indexDb);
+    }
+
+    public byte[] getrange(String key, int startOffset, int endOffset){
+        return this.getrange(key, startOffset, endOffset, 0);
+    }
+
+    public byte[] getrange(byte[] key, int startOffset, int endOffset){
+        return this.getrange(key, startOffset, endOffset, 0);
+    }
 }
+
